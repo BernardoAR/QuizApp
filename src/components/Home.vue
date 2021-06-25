072b483dbfe1b067c22c0f6d0549bdb3434f7d14
Bernardo Alves Roballo <bernardo_roballo@hotmail.com>
2021-06-25 15:46:43 -0300

    Criado visual base das questões.
    Signed-off-by: Bernardo Alves Roballo <bernardo_roballo@hotmail.com>

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-container>
+      <b-card class="col-md-8 mt-5 mx-auto">
+        <b-card-header>
+          <b-row>
+            <b-col sm="1"
+              ><b-button squared variant="primary">1</b-button></b-col
+            >
+          </b-row>
+        </b-card-header>
+        <b-card-body class="text-center">
+          <b-card-title>Question?</b-card-title>
+          <div class="d-grid gap-2 col-10 mx-auto">
+            <b-button squared class="btn-block" variant="outline-primary"
+              >Primary</b-button
+            >
+            <b-button squared class="btn-block" variant="outline-primary"
+              >Primary</b-button
+            >
+            <b-button squared class="btn-block" variant="outline-primary"
+              >Primary</b-button
+            >
+            <b-button squared class="btn-block" variant="outline-primary"
+              >Primary</b-button
+            >
+          </div>
+
+          <b-button class="mt-3" variant="primary" pill>Answer</b-button>
+        </b-card-body>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
